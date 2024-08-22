ac3ed84bf47464947bf87db8cc5cc67c8db3d5d6
MaheesHabibi <maheeshabibi@gmail.com>
2024-08-22 20:14:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
+import discord
 import random
-meme_dict = {
-            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
-            "ROFL": "Tanggapan terhadap lelucon",
-            "SHEESH": "Sedikit ketidaksetujuan",
-            "CREEPY": "Menakutkan, tidak menyenangkan",
-            "AGGRO": "Untuk menjadi lebih agresif/marah"
-            }
-word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
-if word in meme_dict.keys():
-  print(meme_dict[word])
-    # Apa yang harus kita lakukan jika kata itu ditemukan?
-else:
-    print('Kata Tidak Ditemukan!!')
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def guess(ctx):
+    await ctx.send("Guess a number between 1 and 10.")
+
+@bot.command()
+async def answer(ctx, n):
+    if n == random.randint(1, 10):
+        await ctx.send('Nice guessing!!!')
+    else:
+        await ctx.send('Nahhh...., You wrong!!!')
+
+
+            
+bot.run("Rahasia Dong")
+
 
 
   
